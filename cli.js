41181a9f6c7e61a75167d8482c9468a8262ca1d3
David <davidyen1124@gmail.com>
2015-09-04 13:45:15 +0800

    add fbtoken entry point

---
+++ cli.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+var AccessToken = require('./');
+var args = process.argv.splice(2);
+
+if (args.length !== 3) {
+    console.log('Usage: fbtoken <username> <password> <appid>');
+    process.exit(1);
+    return;
+}
+
+var accessToken = new AccessToken(args[0], args[1], args[2]);
+accessToken.loginGetToken(function(err, token) {
+    if (err) throw err;
+
+    console.log(token);
+});
